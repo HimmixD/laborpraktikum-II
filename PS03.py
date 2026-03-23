cd565b767ad527c938a96cddab32038b91e5a183
Himmi <himmelias@gmail.com>
2026-03-23 20:15:07 +0100

    hopefully finished the script

--- PS03.py
+++ PS03.py
@@ -1,23 +1,50 @@
-# %% global imports
+# %% global imports and functions
 import numpy as np
 import matplotlib.pyplot as plt
 from uncertainties import ufloat
 
+def mean_with_uncertainty(values, u_B=0):
+    values = np.array(values)
+    N = len(values)
+
+    mean = np.mean(values)
+    if N > 1:
+        s = np.std(values, ddof=1)          # Stichproben-Stdabw
+        u_A = s / np.sqrt(N)
+    else:
+        u_A = 0
+
+    u_total = np.sqrt(u_A**2 + u_B**2)
+    
+    return ufloat(mean, u_total)
+
+
+
 # %% 1.Generell
 U = ufloat(,)          #Spannung
 I = ufloat(,)          #Strom
+P_zu = U * I            #zugeführte elektrische Leistung
+
 U_cool = ufloat(,)          #Spannung
 I_cool  = ufloat(,)          #Strom
+P_cool = U_cool * I_cool           # ????
+
+f_voll = ufloat(,)           #Leerlauffrequenz
+f_gebremst = ufloat(,)       #Frequenz mit Bremszaum
+
 r = ufloat(,)          #Abstand der Kraftmessung zum Drehpunkt
 F = ufloat(,)          #Kraft
+omega = 2 * np.pi * f_gebremst     #Winkelgeschwindigkeit
+P_motor = r * F * omega     #Motorleistung 
+
 T_1 = ufloat(+273.15,)        #heiße Temperatur
 T_2 = ufloat(+273.15,)        #kalte Temperatur
-W = ufloat(,)           #Arbeit (FLäche in Kurve, ACHTUNG Einheiten)
-f = ufloat(,)           #Leerlauffrequenz
-P_zu = U * I            #zugeführte elektrische Leistung
-P_cool = U_cool * I_cool           # ????
-omega = 2 * np.pi * f     #Winkelgeschwindigkeit
-P_motor = r * F * omega     #Motorleistung 
+
+data = [,,,]          #Flächenintegrale der Kurve (Arbeit)
+dW_B =           #Unsicherheit der Arbeit (Flächenintegral)
+W = mean_with_uncertainty(data, dW_B)           #Arbeit (FLäche in Kurve, ACHTUNG Einheiten)
+
+
 
 
 # %% 1. idealer Wirkungsgrad
@@ -26,7 +53,7 @@ print(f"n_ideal ={n_ideal:.2f}")
 
 
 # %% 1. unbelasteter Wirkungsgrad
-n_unbel = (W * f) / P_zu
+n_unbel = (W * f_voll) / P_zu                        #check das mit den fs nicht, kp was ma da braucchen, stimmt ws?
 print(f"n_unbel ={n_unbel:.2f}")
 
 
@@ -36,7 +63,7 @@ print(f"n_bel ={n_bel:.2f}")
 
 
 # %% 1. mechanischer Wirkungsgrad
-n_motor = P_motor / (W * f)
+n_motor = P_motor / (W * f_voll)                    #check das mit den fs nicht, kp was ma da braucchen, stimmt ws?
 print(f"n_motor ={n_motor:.2f}")
 
 
